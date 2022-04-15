9b2deea1c0582d8c16678cf9671c91c9e237a6c6
JPabloC02 <69996018+JPabloC02@users.noreply.github.com>
2022-04-14 19:17:31 -0500

    Create CombosHelper

---
+++ Concessionaire/Concessionaire/Helpers/ICombosHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Concessionaire.Helpers
+{
+    public interface ICombosHelper
+    {
+        Task<IEnumerable<SelectListItem>> GetComboBrandAsync();
+
+        Task<IEnumerable<SelectListItem>> GetComboCitiesAsync(int stateId);
+
+        Task<IEnumerable<SelectListItem>> GetComboCountriesAsync();
+
+        Task<IEnumerable<SelectListItem>> GetComboStatesAsync(int countryId);
+
+        Task<IEnumerable<SelectListItem>> GetComboVehicleTypesAsync();
+    }
+}
