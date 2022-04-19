5af31bffdbe13cf28de712e2f5e8ee161142cf14
JPabloC02 <69996018+JPabloC02@users.noreply.github.com>
2022-04-18 23:35:58 -0500

    Recover Password

--- Concessionaire/Concessionaire/Helpers/UserHelper.cs
+++ Concessionaire/Concessionaire/Helpers/UserHelper.cs
@@ -85,6 +85,11 @@ namespace Concessionaire.Helpers
             return await _userManager.GenerateEmailConfirmationTokenAsync(user);
         }
 
+        public async Task<string> GeneratePasswordResetTokenAsync(User user)
+        {
+            return await _userManager.GeneratePasswordResetTokenAsync(user);
+        }
+
         public async Task<User> GetUserAsync(string email)
         {
             return await _context.Users
@@ -118,6 +123,11 @@ namespace Concessionaire.Helpers
         public async Task LogoutAsync()
         {
             await _signInManager.SignOutAsync();
+        }
+
+        public async Task<IdentityResult> ResetPasswordAsync(User user, string token, string password)
+        {
+            return await _userManager.ResetPasswordAsync(user, token, password);
         }
 
         public async Task<IdentityResult> UpdateUserAsync(User user)
