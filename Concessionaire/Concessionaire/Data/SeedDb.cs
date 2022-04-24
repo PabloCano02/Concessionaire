b0145f02c7907a1933e09ff94b42d55b7aa4ef70
JPabloC02 <69996018+JPabloC02@users.noreply.github.com>
2022-04-24 12:31:49 -0500

    Finish First Part Project

--- Concessionaire/Concessionaire/Data/SeedDb.cs
+++ Concessionaire/Concessionaire/Data/SeedDb.cs
@@ -8,11 +8,13 @@ namespace Concessionaire.Data
     {
         private readonly DataContext _context;
         private readonly IUserHelper _userHelper;
+        private readonly IBlobHelper _blobHelper;
 
-        public SeedDb(DataContext context, IUserHelper userHelper)
+        public SeedDb(DataContext context, IUserHelper userHelper, IBlobHelper blobHelper)
         {
             _context = context;
             _userHelper = userHelper;
+            _blobHelper = blobHelper;
         }
 
         public async Task SeedAsync()
@@ -22,8 +24,8 @@ namespace Concessionaire.Data
             await CheckCountriesAsync();
             await CheckRolesAsync();
             await CheckVehicleTypesAsync();
-            await CheckUserAsync("1010", "Juan", "Cano", "cano@yopmail.com", "300 422 4861", "Cra 50 # 133 sur 80", UserType.Admin);
-            await CheckUserAsync("2020", "Daniela", "Quintero", "quintero@yopmail.com", "311 515 3245", "Calle 12 # 40 - 20", UserType.User);
+            await CheckUserAsync("1010", "Brad", "Pitt", "brad@yopmail.com", "300 422 4861", "Cra 50 # 133 sur 80", "Brad.jpg", UserType.Admin);
+            await CheckUserAsync("2020", "Angelina", "Jolie", "angelina@yopmail.com", "311 515 3245", "Calle 12 # 40 - 20", "Angelina.jpg", UserType.User);
         }
 
         private async Task<User> CheckUserAsync(
@@ -33,11 +35,13 @@ namespace Concessionaire.Data
             string email,
             string phone,
             string address,
+            string image,
             UserType userType)
         {
             User user = await _userHelper.GetUserAsync(email);
             if (user == null)
             {
+                Guid imageId = await _blobHelper.UploadBlobAsync($"{Environment.CurrentDirectory}\\wwwroot\\images\\users\\{image}", "users");
                 user = new User
                 {
                     FirstName = firstName,
@@ -49,6 +53,7 @@ namespace Concessionaire.Data
                     Document = document,
                     City = _context.Cities.FirstOrDefault(),
                     UserType = userType,
+                    ImageId = imageId,
                 };
 
                 await _userHelper.AddUserAsync(user, "123456");
@@ -56,7 +61,6 @@ namespace Concessionaire.Data
 
                 string token = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
                 await _userHelper.ConfirmEmailAsync(user, token);
-
             }
 
             return user;
@@ -86,7 +90,12 @@ namespace Concessionaire.Data
                                 new City() { Name = "Itagüí" },
                                 new City() { Name = "Envigado" },
                                 new City() { Name = "Bello" },
-                                new City() { Name = "Rionegro" },
+                                new City() { Name = "Sabaneta" },
+                                new City() { Name = "La Ceja" },
+                                new City() { Name = "La Union" },
+                                new City() { Name = "La Estrella" },
+                                new City() { Name = "Copacabana" },
+                                new City() { Name = "Caldas" },
                             }
                         },
                         new State()
@@ -98,6 +107,44 @@ namespace Concessionaire.Data
                                 new City() { Name = "Santa fe" },
                                 new City() { Name = "Usme" },
                                 new City() { Name = "Bosa" },
+                            }
+                        },
+                        new State()
+                        {
+                            Name = "Valle",
+                            Cities = new List<City>() {
+                                new City() { Name = "Santiago de Cali" },
+                                new City() { Name = "Jumbo" },
+                                new City() { Name = "Jamundí" },
+                                new City() { Name = "Chipichape" },
+                                new City() { Name = "Buenaventura" },
+                                new City() { Name = "Cartago" },
+                                new City() { Name = "Buga" },
+                                new City() { Name = "Palmira" },
+                            }
+                        },
+                        new State()
+                        {
+                            Name = "Santander",
+                            Cities = new List<City>() {
+                                new City() { Name = "Bucaramanga" },
+                                new City() { Name = "Málaga" },
+                                new City() { Name = "Barrancabermeja" },
+                                new City() { Name = "Rionegro" },
+                                new City() { Name = "Barichara" },
+                                new City() { Name = "Zapatoca" },
+                            }
+                        },
+                        new State()
+                        {
+                            Name = "Atlantico",
+                            Cities = new List<City>() {
+                                new City() { Name = "Barranquilla" },
+                                new City() { Name = "Puerto Colombia" },
+                                new City() { Name = "Soledad" },
+                                new City() { Name = "Malambo" },
+                                new City() { Name = "Candelaria" },
+                                new City() { Name = "Sabanalarga" },
                             }
                         },
                     }
@@ -129,8 +176,75 @@ namespace Concessionaire.Data
                                 new City() { Name = "El Paso" },
                             }
                         },
+                        new State()
+                        {
+                            Name = "California",
+                            Cities = new List<City>() {
+                                new City() { Name = "Los Angeles" },
+                                new City() { Name = "San Francisco" },
+                                new City() { Name = "San Diego" },
+                                new City() { Name = "San Bruno" },
+                                new City() { Name = "Sacramento" },
+                                new City() { Name = "Fresno" },
+
+                            }
+                        },
+                        new State()
+                        {
+                            Name = "Massachusetts",
+                            Cities = new List<City>() {
+                                new City() { Name = "Boston" },
+                                new City() { Name = "Springfield" },
+                                new City() { Name = "Newton" },
+                                new City() { Name = "Revere" },
+                                new City() { Name = "Cambridge" },
+                                new City() { Name = "Braintree" },
+
+                            }
+                        },
                     }
                 });
+                _context.Countries.Add(new Country
+                {
+                    Name = "Ecuador",
+                    States = new List<State>()
+                    {
+                        new State()
+                        {
+                            Name = "Pichincha",
+                            Cities = new List<City>() {
+                                new City() { Name = "Quito" },
+                                new City() { Name = "Cayambe" },
+                                new City() { Name = "Mejía" },
+                                new City() { Name = "Rumiñahui" },
+                                new City() { Name = "San Miguel de Los Bancos" },
+                            }
+                        },
+                        new State()
+                        {
+                            Name = "Esmeraldas",
+                            Cities = new List<City>() {
+                                new City() { Name = "Esmeraldas" },
+                                new City() { Name = "Atacames" },
+                                new City() { Name = "Eloy Alfaro" },
+                                new City() { Name = "Muisne" },
+                                new City() { Name = "San Lorenzo" },
+                            }
+                        },
+                        new State()
+                        {
+                            Name = "Carchi",
+                            Cities = new List<City>() {
+                                new City() { Name = "Tulcán" },
+                                new City() { Name = "Bolívar" },
+                                new City() { Name = "Espejo" },
+                                new City() { Name = "Mira" },
+                                new City() { Name = "Montúfar" },
+                            }
+                        },
+                    }
+                });
+
             }
 
             await _context.SaveChangesAsync();
