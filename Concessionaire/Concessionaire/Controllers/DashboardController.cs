1a5470bc174f00f85d3dc17bd9bc4f3accd2b8c5
JPabloC02 <69996018+JPabloC02@users.noreply.github.com>
2022-05-31 19:43:24 -0500

    Add Dashboard

---
+++ Concessionaire/Concessionaire/Controllers/DashboardController.cs
@@ -0,0 +1,35 @@
+﻿using Concessionaire.Data;
+using Concessionaire.Enums;
+using Concessionaire.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Concessionaire.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+
+        public DashboardController(DataContext context, IUserHelper userHelper)
+        {
+            _context = context;
+            _userHelper = userHelper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.UsersCount = _context.Users.Count();
+            ViewBag.VehiclesCount = _context.Vehicles.Count();
+            ViewBag.NewOrdersCount = _context.Reserves.Where(o => o.OrderStatus == OrderStatus.Nuevo).Count();
+            ViewBag.ConfirmedOrdersCount = _context.Reserves.Where(o => o.OrderStatus == OrderStatus.Confirmado).Count();
+            ViewBag.CanceledOrdersCount = _context.Reserves.Where(o => o.OrderStatus == OrderStatus.Cancelado).Count();
+
+            return View(await _context.TemporalReserves
+                    .Include(tr => tr.User)
+                    .Include(tr => tr.Vehicle)
+                    .ToListAsync());
+        }
+    }
+}
